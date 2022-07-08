e80a943f0872a5005b30d7d43393069361395ce9
Verónica Bellido <veronica.bellido.sm@gmail.com>
2022-07-08 13:33:10 +0200

    ejercicio alteracion tablas

---
+++ modulo1-ejercicio3-alteracion-tablas.sql
@@ -0,0 +1,15 @@
+ALTER TABLE zapatillas
+ADD COLUMN marca VARCHAR(45) NOT NULL,
+ADD COLUMN talla INT NOT NULL;
+
+ALTER TABLE empleados
+MODIFY COLUMN salario FLOAT NOT NULL;
+
+ALTER TABLE clientes
+DROP COLUMN pais,
+MODIFY COLUMN codigo_postal INT(5);
+
+ALTER TABLE facturas
+ADD COLUMN total FLOAT;
+
+
